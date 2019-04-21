5c528cb87eef8f845829d290671964fe062bca44
olivier M-L <mlouise.olivier@gmail.com>
2019-04-21 19:41:43 +0200

    init stryker tests

--- src/Service/Api/JetpackApi.test.js
+++ src/Service/Api/JetpackApi.test.js
@@ -1,11 +1,8 @@
 const JetpackApi = require('./JetpackApi');
 const Jetpack = require('../../Entity/Jetpack');
 
-describe('JetpackApi  get Jetpacks', function () {
-
-    test('Test JetPack features', () => {
-
-        expect.assertions(6);
+describe('JetpackApi features', function () {
+    test('Test get Jetpacks features', () => {
 
         let httpClientMock = {
             fetch: jest.fn()
@@ -15,25 +12,26 @@ describe('JetpackApi  get Jetpacks', function () {
             {
                 id: "123",
                 name: "The Jetpack",
-                image: "base64 ..."
+                image: "base64...",
             }
         ]);
+
+        expect.assertions(6);
 
         let jetpackApi = new JetpackApi(httpClientMock);
         jetpackApi.getJetpacks().then(resp => {
             expect(Array.isArray(resp)).toBe(true);
             expect(resp.length).toBe(1);
-            expect(resp[0]).toBeInstanceOf(Jetpack);
             expect(resp[0].id).toBe("123");
             expect(resp[0].name).toBe("The Jetpack");
-            expect(resp[0].image).toBe("base64 ...");
+            expect(resp[0].image).toBe("base64...");
+            expect(resp[0]).toBeInstanceOf(Jetpack);
+        }).catch( (e) => {
+            fail(e);
         });
     });
 
     test('Test Create Jetpack',() =>{
-
-        expect.assertions(4);
-
         let httpClientMock = {
             fetch: jest.fn()
         };
@@ -46,12 +44,17 @@ describe('JetpackApi  get Jetpacks', function () {
             }
         );
 
+        expect.assertions(4);
+
         let jetpackApi = new JetpackApi(httpClientMock);
-        jetpackApi.createJetPack('x','y').then(resp => {
-            expect(resp).toBeInstanceOf(Jetpack);
-            expect(resp.id).toBe("124");
-            expect(resp.name).toBe("My Jetpack");
-            expect(resp.image).toBe("base64...");
+        jetpackApi.createJetPack('x','y').then(
+            function (resp) {
+                expect(resp.id).toBe("124");
+                expect(resp.name).toBe("My Jetpack");
+                expect(resp.image).toBe("base64...");
+                expect(resp).toBeInstanceOf(Jetpack);
+            }).catch((e) => {
+            fail(e)
         });
-    });
+    })
 });
